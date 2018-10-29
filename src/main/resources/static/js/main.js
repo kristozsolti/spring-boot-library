dce8287ce610b5176be775bdc921e9863840b1c5
Kristo Zsolt <kristo.zsolti@gmail.com>
2018-10-29 12:04:52 +0100

    rename header.js to main.js and enabled toolbars everywhere + some design change in book view

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -2,7 +2,12 @@ $( document ).ready( function() {
 	
 	// Toggle active nav-link by link when the document is loading	
 	var active = window.location.pathname;
-	$("a[href|='" + active + "']").addClass("active")
+	$("a[href|='" + active + "']").addClass("active");
+
+	// Enable Tooltips everywhere
+	$(function () {
+		$('[data-toggle="tooltip"]').tooltip()
+	});
 			
 });
 
