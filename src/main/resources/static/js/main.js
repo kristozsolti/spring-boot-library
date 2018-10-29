93764e8b5e02762cd4332c00298c8b07206ae26f
Kristo Zsolt <kristo.zsolti@gmail.com>
2018-10-29 20:27:04 +0100

    minor design update

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -2,7 +2,8 @@ $( document ).ready( function() {
 	
 	// Toggle active nav-link by link when the document is loading	
 	var active = window.location.pathname;
-	$("a[href|='" + active + "']").addClass("active");
+	var activeLink = $("a[href='" + active + "']:first");
+	activeLink.addClass("active");
 
 	// Enable Tooltips everywhere
 	$(function () {
